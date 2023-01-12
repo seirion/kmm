d19df2f91830e14b73672f0c7874ecb74311496e
babo.neo <seirion@mensakorea.org>
2023-01-12 10:23:38 +0900

    add Docs plugins

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.seirion.kmm"
-version = "0.0.12"
+version = "0.0.13"
 
 val GITHUB_USER: String by project
 val GITHUB_TOKEN: String by project
@@ -21,7 +21,7 @@ kotlin {
     cocoapods {
         summary = "Some description for the Shared Module"
         homepage = "Link to the Shared Module homepage"
-        version = "0.0.12"
+        version = "0.0.13"
         ios.deploymentTarget = "14.1"
         framework {
             baseName = "shared"
